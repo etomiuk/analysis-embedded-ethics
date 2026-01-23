9d24ad62df627fa8ad525332f9295c4a4e1d5d2e
etomiuk <emma.tomiuk@mail.mcgill.ca>
2026-01-22 20:51:30 -0500

    anovas for attitudes per course

--- scripts/utils.py
+++ scripts/utils.py
@@ -37,11 +37,23 @@ def likert_plot_attitudes(ee_data, cg_data, attitude):
     fig.legend(handles, likert_values, bbox_to_anchor=(1.34, 0.90))
     plt.show()
 
+
 # function for checking equal variance between two groups of data
-def equal_var_test(ee_data, cg_data, var_list):
+def equal_var_test(data, var_list):
+    '''
+    Docstring for equal_var_test
+    
+    :param data: a list of the dataframes for which we want to get the variance of.
+    :param var_list: Description
+    '''
+    # put data in correct format
+
     equal_var = pd.DataFrame()
     for var in var_list:
-        equal_var_test = pg.homoscedasticity(data=[ee_data[var].to_numpy(), cg_data[var].to_numpy()], method="levene")
+        data_formatted = []
+        for d in data:
+            data_formatted.append(d[var].to_numpy())
+        equal_var_test = pg.homoscedasticity(data=data_formatted, method="levene")
         equal_var = pd.concat([equal_var, equal_var_test])
 
     equal_var.index = var_list # change index to be the attitudes
@@ -75,6 +87,52 @@ def ttest(ee_data, cg_data, var_list):
     mwu_avg.insert(3, "significance", mwu_stars)
 
     return ttest_avg, mwu_avg
+
+
+def anova(data, groups, var_list):
+    '''
+    Performs both the parametric and non-parametric anova and returns them in that order
+    '''
+    aov = pd.DataFrame()
+    welch = pd.DataFrame()
+    aov_stars = []
+    welch_stars = []
+
+    for var in var_list:
+        aov_var = pg.anova(data=data, dv=var, between=groups)
+        welch_var = pg.welch_anova(data=data, dv=var, between=groups)
+
+        aov_stars.append(p_val_star(aov_var["p-unc"].values[0])) # take index 0 cause only one element at a time
+        welch_stars.append(p_val_star(welch_var["p-unc"].values[0])) # take index 0 cause only one element at a time
+
+        aov = pd.concat([aov, aov_var])
+        welch = pd.concat([welch, welch_var])
+
+    # set the index as the attitudes
+    aov.index = var_list
+    welch.index = var_list
+
+    aov.insert(4, "significance", aov_stars)
+    welch.insert(3, "significance", welch_stars)
+
+    return aov, welch
+
+def anova_post_hoc(data, groups, var_list, aov_data, parametric=True):
+    posthoc_list = {}
+    stars = []
+    for var in var_list:
+        if aov_data["p-unc"][var] < 0.05:
+            if parametric:
+                posthoc = pg.pairwise_tukey(data=data, dv=var, between=groups)
+            else:
+                posthoc = pg.pairwise_gameshowell(data=data, dv=var, between=groups)
+
+            stars = posthoc["pval"].apply(p_val_star)
+            #stars.append(p_val_star(posthoc["pval"])) # take index 0 cause only one element at a time
+            posthoc.insert(9, "significance", stars)
+            posthoc_list[var] = posthoc
+
+    return posthoc_list
 
 
 # function for converting p-values to stars
